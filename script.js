a284fd91748e995b09b6764932c4ad42e8477083
siseldyke <siseldyke@gmail.com>
2024-09-27 13:33:21 -0400

    search for name working

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //variables
-
+apiKey = (`RGAPI-fee3b467-805d-466b-b56f-8d627ab37e87`)
 
 const button = document.querySelector('#submitButton')
 const input = document.querySelector('#textInput')
 const nameContainer = document.querySelector('#champName')
-const tempContainer = document.querySelector('#champTitle')
+const titleContainer = document.querySelector('#champTitle')
 const imageContainer = document.querySelector('#champImage')
 
 
@@ -14,10 +14,13 @@ const imageContainer = document.querySelector('#champImage')
 // functions
 
 button.addEventListener('click', async () => {
-    
+    let name = input.value
     let response = await axios.get(
-        `https://ddragon.leagueoflegends.com/cdn/14.19.1/data/en_US/champion.json`
+        `https://ddragon.leagueoflegends.com/cdn/14.19.1/data/en_US/champion.json?key=${apiKey}${name}`
     )
     console.log(response)
+
+    let champName = response.data.data[name].id
+    nameContainer.textContent = (champName)
 })
 
