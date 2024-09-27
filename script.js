4fdee62023e89e023bd3b9df6df97adf898469f0
siseldyke <siseldyke@gmail.com>
2024-09-27 12:08:28 -0400

    second commit searchbar works

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 //variables
 
 
-
+const button = document.querySelector('#submitButton')
+const input = document.querySelector('#textInput')
+const nameContainer = document.querySelector('#champName')
+const tempContainer = document.querySelector('#champTitle')
+const imageContainer = document.querySelector('#champImage')
 
 
 
 
 
 // functions
+
+button.addEventListener('click', async () => {
+    
+    let response = await axios.get(
+        `https://ddragon.leagueoflegends.com/cdn/14.19.1/data/en_US/champion.json`
+    )
+    console.log(response)
+})
+
